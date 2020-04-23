76019a5de2294539cb9c95bd677e3c0c395d3387
Rico Roma <ricoroma14@gmail.com>
2020-04-23 15:57:11 -0300

    Class Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,36 @@
+package boardgame;
+
+public class Position {
+	protected int row;
+	protected int column;
+	
+	public Position() {
+		
+	}
+	
+	public Position(int row, int column) {
+		this.setRow(row);
+		this.setColumn(row);
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getRow() + ", " + this.getColumn();
+	}
+}
